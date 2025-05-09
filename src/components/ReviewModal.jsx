fa00e09c5c69b707917334174cfa7e724cf79614
EunBin <rhdmsqls1234@naver.com>
2025-05-10 01:02:58 +0900

    modify review modal

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -9,17 +9,18 @@ export default function ReviewModal({onClose}){
                         <VscChromeClose/>
                     </button>
                 </div>
-            </div>
+            
 
-            <div className="review_text">제휴 업체 리뷰</div>
-            <div className="rating_wrap_inmodal">
-                <div className="rating_label">별점</div>
+                <div className="review_text">제휴 업체 리뷰</div>
+                <div className="rating_wrap_inmodal">
+                    <div className="rating_label">별점</div>
+                </div>
+                <div className="rating_content_wrap_inmodal">
+                    <div className="review_content_label">리뷰 내용</div>
+                    <input className="review_content" type="text"/>
+                </div>
+                <button className="add_review_btn_inmodal">리뷰 등록</button>
             </div>
-            <div className="rating_content_wrap_inmodal">
-                <div className="review_content_label">리뷰 내용</div>
-                <input className="review_content" type="text"/>
-            </div>
-            <button className="add_review_btn_inmodal">리뷰 등록</button>
         </div>
     );
 }
